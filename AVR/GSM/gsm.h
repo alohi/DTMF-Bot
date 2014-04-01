9d059a2a8bbcfc226d825e3caf49fc65e3e7494c
Sriharsha <helpzuna@gmail.com>
2014-04-01 18:21:16 +0530

    small changes
    Signed-off-by: Sriharsha <helpzuna@gmail.com>

--- AVR/GSM/gsm.h
+++ AVR/GSM/gsm.h
@@ -2,8 +2,15 @@
 #ifndef __gsm_h__
 #define __gsm_h__
 
-extern unsigned char getDtmf(void);
+#include "uart.h"
+#include "timer.h"
 
+extern unsigned char getDtmf(void);
+extern unsigned char enableDtmf(void);
+extern unsigned char getDtmf(void);
+extern unsigned char connectCall(void);
+extern unsigned char detectCall(void);
+extern unsigned char detectModem(void);
 
 
 #endif
